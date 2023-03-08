1f32cd30ed18d0932f52b871005d57207a35aee5
RaltsHunter <117216708+RaltsHunter@users.noreply.github.com>
2023-03-08 16:06:27 -0600

    The rest of the project

--- midterm/Assets/playerScript.cs
+++ midterm/Assets/playerScript.cs
@@ -9,11 +9,13 @@ public class playerScript : MonoBehaviour
     public float mSpeed;
     public float rSpeed;
     //time remaining
-    float remainTime = 60;
     //counters and display
     float goldEat = 0;
+    float timeS = 60;
     public TMP_Text goldCounter;
     public TMP_Text timer;
+    //frasier bombs
+    public GameObject frasier;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,14 @@ public class playerScript : MonoBehaviour
         float hAxis = Input.GetAxis("Horizontal");
         gameObject.transform.Translate(gameObject.transform.forward * Time.deltaTime * mSpeed * vAxis, Space.World);
         gameObject.transform.Rotate(0, rSpeed * Time.deltaTime * hAxis, 0);
+        //countdown
+        timeS = 60 - Time.time;
+        timer.text = timeS.ToString() + " Seconds Left";
+        if (timeS <= 0)
+        {
+            timer.text = "GAME OVER";
+        }
+
     }
     //robot eating gold
     private void OnTriggerEnter(Collider other)
@@ -38,8 +48,26 @@ public class playerScript : MonoBehaviour
         {
             Destroy(other.gameObject);
             goldEat = goldEat + 1;
-            goldCounter.text = goldEat.ToString();
+            goldCounter.text = goldEat.ToString() + " Gold Fuel Consumed";
+
 
         }
+        if (goldEat >= 10)
+        {
+            goldCounter.text = "Victory!";
+        }
+
+        //robot running into wine, will generate Frasier Cubes
+        if (other.CompareTag("wine"))
+        {
+            Destroy(other.gameObject);
+            for (int i = 0; i < 4; i++)
+            {
+                float xPos = Random.Range(-50, 50);
+                float zPos = Random.Range(-50, 50);
+                GameObject Fraj = Instantiate(frasier, new Vector3(xPos, 20, zPos), transform.rotation);
+            }
+        }
     }
+    
 }
